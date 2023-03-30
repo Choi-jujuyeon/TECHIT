eef52e4bc5048b3e968f3219fb4e0e4b2af8770c
Choi-jujuyeon <chlwndus1216@naver.com>
2023-03-30 13:58:59 +0900

    react

--- dapp/src/App.jsx
+++ dapp/src/App.jsx
@@ -1,7 +1,15 @@
+import "./index.css";
+
+// import "./App_bak.css";
+
 function App() {
     return (
         <div>
-            <h1>HELLO !REACT</h1>
+            {/* <h1 className="hello">HELLO !REACT</h1> */}
+            <h1 className="text-3xl font-bold ">HELLO !REACT</h1>
+
+            <h1 className="text-3xl font-bold underline">My name is JU</h1>
+            <h1 className="text-3xl font-bold">Favorite</h1>
         </div>
     );
 }
